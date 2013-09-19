3b7cc53837e8efcb0229f25783d4affb2875360b
rren88 <ric.ren88@gmail.com>
2013-09-19 11:06:13 -0700

    We started our project Timebord. Cheers!
    In this commit:
    1. created Timeboard-app project.

---
+++ Timeboard-web/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeboardWeb::Application.config.secret_key_base = '7e6cae9cf7ca271abf7f544d19f12faf3ad4c82454fe48e60a544b4fcb8d6ff846bdde813b7c6c762ef9b277b4adcb7f3542906e46b0a4316a84379575ec5b1b'
