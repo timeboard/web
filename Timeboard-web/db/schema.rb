6755b9b3bb3b68e5d875ff23c784b704e538e413
rren88 <ric.ren88@gmail.com>
2013-09-26 15:34:39 -0700

    Config local env

--- Timeboard-web/db/schema.rb
+++ Timeboard-web/db/schema.rb
@@ -41,6 +41,6 @@ ActiveRecord::Schema.define(version: 20130926081244) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
