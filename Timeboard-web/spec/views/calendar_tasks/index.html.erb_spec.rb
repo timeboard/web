bb61d3aa8c70e0194c3390ae1b739b78770cec07
crazymooner <crazymooner@gmail.com>
2013-09-26 05:36:46 +0000

    fix test

--- Timeboard-web/spec/views/calendar_tasks/index.html.erb_spec.rb
+++ Timeboard-web/spec/views/calendar_tasks/index.html.erb_spec.rb
@@ -5,19 +5,17 @@ describe "calendar_tasks/index" do
     assign(:calendar_tasks, [
       stub_model(CalendarTask,
         :user_id => 1,
-        :type => "Type",
         :item_class => "Item Class",
         :summary => "Summary",
-        :description => "MyText",
+        :description => "Desc",
         :url => "MyText",
         :repeat_rule => "Repeat Rule"
       ),
       stub_model(CalendarTask,
         :user_id => 1,
-        :type => "Type",
         :item_class => "Item Class",
         :summary => "Summary",
-        :description => "MyText",
+        :description => "Desc",
         :url => "MyText",
         :repeat_rule => "Repeat Rule"
       )
@@ -28,10 +26,9 @@ describe "calendar_tasks/index" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => "Type".to_s, :count => 2
     assert_select "tr>td", :text => "Item Class".to_s, :count => 2
     assert_select "tr>td", :text => "Summary".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "Desc".to_s, :count => 2
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
     assert_select "tr>td", :text => "Repeat Rule".to_s, :count => 2
   end
