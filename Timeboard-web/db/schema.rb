5d326385402a8597f462a9f4e475f84a70598163
crazymooner <crazymooner@gmail.com>
2013-10-14 18:03:51 +0000

    fullcalender

--- Timeboard-web/db/schema.rb
+++ Timeboard-web/db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20131003044347) do
     t.string   "remember_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
---
+++ Timeboard-web/spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    first_name  "Tao"
+    last_name   "Liu"
+    email       "crazymooner@gmail.com"
+    password    "taotao"
+    password_confirmation "taotao"
+  end
+end
